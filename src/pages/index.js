79f451f4e7bc6d4d9e53ae911190593e578b9392
michalwanto <59409764+michalwanto@users.noreply.github.com>
2020-12-13 09:17:25 +1100

    test extra pict

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,7 @@ const IndexPage = () => (
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
+      <img className="background1" src="https://content.screencast.com/users/Michalwnt/folders/Default/media/53ba4ba0-0c49-4f55-aa1e-94fbf1be295d/BBWebsite1%202.png"></img>
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
