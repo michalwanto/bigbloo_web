6e7def9d93fa37518bd54ef1bbba078b3cb90938
michalwanto <59409764+michalwanto@users.noreply.github.com>
2020-12-20 10:57:46 +1100

    fixed the blog pages

---
+++ src/posts/react copy.md
@@ -0,0 +1,6 @@
+---
+title: "React copy"
+date: "2020-12-15"
+---
+
+This is the page about React
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,43 @@
 import React from 'react'
 import Layout from "../components/layout"
+import {graphql} from "gatsby"
 
-const blogTemplate = () =>{
+import blogStyles from "./blog.module.scss"
+
+export const query = graphql`
+query(
+  $slug:String!
+){
+markdownRemark(
+  fields:{
+    slug:{
+      eq:$slug
+    }
+  }
+){
+  excerpt
+  html
+  timeToRead
+  frontmatter{
+    title
+    date
+  }
+  fields{
+    slug
+  }
+}
+}
+`
+
+const blogTemplate = (props) =>{
+
     return(
         <Layout>
-            This is the template blog page
+        <div className={blogStyles.container}>
+            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+            <h6>{props.data.markdownRemark.frontmatter.date}</h6>
+            <p dangerouslySetInnerHTML={{__html:props.data.markdownRemark.html}}></p>
+            </div>
         </Layout>
     )
 }
